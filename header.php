fbf6357951665f248538c875c26588b77c8bb331
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-24 18:00:10 -0500

    Update header.php

--- header.php
+++ header.php
@@ -10,27 +10,6 @@ integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6
 <title>Homework 2</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
-<div class="container-fluid">
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-<ul class="navbar-nav">
-<li class="nav-item">
-<a class="nav-link active" aria-current="page" href="#">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Features</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Pricing</a>
-</li>
-</ul>
-</div>
-</div>
-</nav>
+
 </body>
 </html>
