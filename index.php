df22415c4c77904545239af2c5de4bedb0e4e4c3
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-24 17:57:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <!doctype html>
 <html lang = "en">
 <body>
-<h1 style="color:blue; text-align:center;">Welcome to Amir's Homework 2!</h1>
-<p style="color:red; text-align:center;">Please enter your Name and Email Address please!</p>
+<h1 >Welcome to Amir's Homework 2!</h1>
+<p >Please enter your Name and Email Address please!</p>
 <br></br>
-<p style="text-align:center;">
-<h2 style="text-align:center;"> POST - Function </h2>
+<p >
+<h2 > POST - Function </h2>
 <form action="handlepost.php" method="post">
-<p style="text-align:center;"> Name: <input type="text" name="name"></p><br>
-<p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br>
-<p style="text-align:center;"><input type="submit"></p>
+<p > Name: <input type="text" name="name"></p><br>
+<p > E-mail: <input type="text" name="email"></p><br>
+<p "><input type="submit"></p>
 <br></br>
-<h2 style="text-align:center;"> GET - Function </h2>
+<h2 > GET - Function </h2>
 <form action="welcome_get.php" method="get">
-<p style="text-align:center;"> Name: <input type="text" name="name"></p><br>
-<p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br>
-<p style="text-align:center;"><input type="submit"></p>
+<p > Name: <input type="text" name="name"></p><br>
+<p > E-mail: <input type="text" name="email"></p><br>
+<p ><input type="submit"></p>
 <br></br>
 </form>
 </form>
