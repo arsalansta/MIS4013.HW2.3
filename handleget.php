05ee61b0660cc12817aefbe1fc4a59ef401bd10a
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-29 02:01:28 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -13,6 +13,7 @@
   
   
   <div class="center"><h1> GET RESPONSE </h1></div>
+  </br>
   
 <div class="center">Welcome <?php echo $_GET["name"]; ?><br>
   Your email address is: <?php echo $_GET["email"]; ?></div>
