7aef31b9654194a3625e76f18e58468d0f8a9ef8
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-29 00:29:46 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,5 +1,9 @@
 <html>
 <body>
+  
+
+  <a href="mis.hw2.6.amiresta.oucreate.com">Main Page</a>
+  
 Welcome <?php echo $_POST["name"]; ?><br>
 your email address is: <?php echo $_POST["email"]; ?>
 </body>
