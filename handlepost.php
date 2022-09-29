273cf049630453e9102f52c04ea2d5fb051dd7fd
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-29 02:06:44 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -10,11 +10,11 @@
 
 	  
 
-  <div class="center"><h1> Get RESPONSE </h1></div>
+  <div class="center"><h1> Post RESPONSE </h1></div>
   
     </br>
 	<div class="center">Welcome <?php echo $_POST["name"]; ?>
-	<br>
+	
 	your email address is: <?php echo $_POST["email"]; ?></div>
 </body>
 </html>
