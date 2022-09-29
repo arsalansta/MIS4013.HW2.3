cc44a350cc07a0e2669128c40dbb7b1e094c0850
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-29 00:30:03 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -1,5 +1,8 @@
 <html>
 <body>
+  
+  <a href="mis.hw2.6.amiresta.oucreate.com">Main Page</a>
+  
 <h1> GET RESPONSE </h1>
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
