c08e2db9a0db557126956dc9e586b629c0ed7b94
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-29 00:43:59 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -5,18 +5,11 @@
   
 
   
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Main Page</title>
-  </head>
-  <body>
+
     <button onclick="href='mis.hw2.6.amiresta.oucreate.com';">
     Main Page 
     </button>
-  </body>
-</html>
-  
+
   
 <h1> GET RESPONSE </h1>
 Welcome <?php echo $_GET["name"]; ?><br>
