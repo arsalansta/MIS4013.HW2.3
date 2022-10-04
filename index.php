85acf27b24f32a240c148c0a0d715ce551cf2b2a
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-04 14:07:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,45 +9,30 @@
 <h1 class="center" >Welcome to Amir's Homework 2!</h1>
   
 
+  
 
-        <a class="nav-link" href="course-section.php">course-section</a>
-    
-		
-	
-        <a class="nav-link" href="courses.php">Courses</a>
-    
-	
-	
-        <a class="nav-link" href="instructor-cards.php">instructor cards</a>
-    
-	
-        <a class="nav-link" href="instructor-section.php">instructor-section</a>
-    
-		
+  </br>
 
-        <a class="nav-link" href="instructors.php">instructors</a>
 
-		
-		
-	
-        <a class="nav-link" href="sections.php">sections</a>
-    
+<div class="center"> <p>Please enter your Name and Email Address please!</p> 
+</div>
 
   </br>
 
 
 
 
-
-
-
-
+<div class="center">
+  <div class="row">
+    <div class="col">
+      
+      
     
       
       <h2 > POST - Function </h2>
 <form action="handlepost.php" method="post">
-<p > Instructor Name: <input type="text" name="instructor-name"></p><br>
-<p > Course_ID: <input type="text" name="cours-id"></p><br>
+<p > Name: <input type="text" name="name"></p><br>
+<p > E-mail: <input type="text" name="email"></p><br>
 <p "><input type="submit">
                          </p>
     </div>
@@ -55,19 +40,41 @@
                          
                          
                          
-   
+    <div class="col">
+                    
+
+                    
+      <h2 > GET - Function </h2>
+                         
+<form action="handleget.php" method="get">
+<p > Name: <input type="text" name="name"></p><br>
+<p > E-mail: <input type="text" name="email"></p><br>
+<p ><input type="submit"></p>
+                        
+             </form>  
       
       
 
       
+    </div>
+    
+  </div>
+
+</div>
+
+
+
+
+
+
+
+<p >
   
 
-
-
-
-
-
-
+                         
+<br></br>
+           
+<br></br>
 
 
 </html>
